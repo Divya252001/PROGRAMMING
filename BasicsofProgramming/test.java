aa725883d0b2d140280264081de2fd5e63774c10
Divya252001 <120187361+Divya252001@users.noreply.github.com>
2022-12-09 18:45:32 +0530

    Add files via upload

---
+++ BasicsofProgramming/test.java
@@ -0,0 +1,94 @@
+Class test
+{
+void m1(char ch)
+{
+System.out.println("Character arg method");
+}
+Void m1(String str)
+{
+System.out.println("String arg method");
+}
+Void m1(int i,int j)
+{
+System.out.println("i+***"+j);
+}
+Public Static Void main(String[]args)
+{
+test t=new test();
+t.m1('b');
+t.m1("java");
+t.m1(17,45);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
